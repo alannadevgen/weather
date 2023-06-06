cb3d8f98054e3be89777b54eb006cfc862013b21
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-06-06 23:29:04 +0200

    Test credentials

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from weather.weather_records import WeatherRecords
+from google.oauth2 import service_account
+
 
 if __name__ == "__main__":
-    weather_records = WeatherRecords()
-    weather_records.run()
-    records = weather_records.get_records()
+    credentials = service_account.Credentials.from_service_account_file("credentials.json")
+    print(credentials)
+
+    # weather_records = WeatherRecords()
+    # weather_records.run()
+    # records = weather_records.get_records()
+    # print(records)
+    # records.to_gbq(
+    #     destination_table="weather.weather",
+    #     project_id="weather-prediction-388920",
+    #     if_exists="append",
+    #     credentials=credentials
+    # )
+    records.to_csv("weather_records.csv")
