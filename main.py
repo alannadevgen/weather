c78e6fb916caa19b35251d310b211b075f4c6a7e
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-06-16 22:14:54 +0200

    Adding environment variables for bucket info

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
 from weather.weather_records import WeatherRecords
 
 if __name__ == "__main__":
+    load_dotenv()
     weather_records = WeatherRecords(
         upload_to_bq=True,
-        dataset="weather",
-        table_name="weather",
-        project_id="weather-prediction-388920",
+        dataset=os.environ.get("DATASET"),
+        table_name=os.environ.get("TABLE_NAME"),
+        project_id=os.environ.get("PROJECT_ID"),
     )
     weather_records.run()
